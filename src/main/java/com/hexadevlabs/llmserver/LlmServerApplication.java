d9a264fd57439d02bf29718e8803a43b22b1df65
felix <felix@zaslavskiy.net>
2023-06-10 04:58:27 -0400

    init

---
+++ src/main/java/com/hexadevlabs/llmserver/LlmServerApplication.java
@@ -0,0 +1,13 @@
+package com.hexadevlabs.llmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexadevlabs/llmserver/LlmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexadevlabs.llmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
