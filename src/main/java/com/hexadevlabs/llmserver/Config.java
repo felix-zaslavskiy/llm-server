9ab598dc4190efcd752fb589852ababbc4c678b0
felix <felix@zaslavskiy.net>
2023-06-12 12:44:18 -0400

    small cleanups

--- src/main/java/com/hexadevlabs/llmserver/Config.java
+++ src/main/java/com/hexadevlabs/llmserver/Config.java
@@ -2,7 +2,6 @@ package com.hexadevlabs.llmserver;
 
 import com.hexadevlabs.gpt4all.LLModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,16 +15,6 @@ public class Config {
 
     @Bean
     LLModelService llModelService(){
-
-        // Alt path for WSL Linux
-        //String baseModelPath = "/mnt/c/Users/felix/AppData/Local/nomic.ai/GPT4All/";
-
-        // Optionally in case override to location of shared libraries is necessary
-        //LLModel.LIBRARY_SEARCH_PATH = "C:\\Users\\felix\\gpt4all\\lib\\";
-
-        // Debut output format. In case you need it.
-        // LLModel.OUTPUT_DEBUG=true;
-
         LLModelService llModelService = new LLModelService(getLLModel());
         llModelService.setLlmDebugMode(yamlConfig.getLlmDebugMode());
         return llModelService;
